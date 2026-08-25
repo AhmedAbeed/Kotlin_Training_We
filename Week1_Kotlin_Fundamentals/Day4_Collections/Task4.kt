37239197b496b861db74febdd9e8e9d13fd643a7
Ahmed Abed <a.abed.h20@gmail.com>
2026-08-25 16:00:08 +0300

    Update Task4.kt

--- Week1_Kotlin_Fundamentals/Day4_Collections/Task4.kt
+++ Week1_Kotlin_Fundamentals/Day4_Collections/Task4.kt
@@ -1,1 +1,32 @@
+fun main()
+{
+    val laptops = listOf(
+        Laptop("hp", 2000),
+        Laptop("lenovo", 3000),
+        Laptop("dell", 5000),
+        Laptop("hp", 7000),
+        Laptop("mac", 10000),
+        Laptop("asus", 4500),
+        Laptop("acer", 3500),
+        Laptop("msi", 8000),
+        Laptop("samsung", 6000),
+        Laptop("huawei", 5500),
+        Laptop("toshiba", 4000),
+        Laptop("razer", 12000),
+        Laptop("microsoft", 9000),
+        Laptop("lg", 6500),
+        Laptop("hp", 1000),
+        Laptop("lenovo", 18000),
+        Laptop("dell", 25000),
+        Laptop("asus", 30000),
+        Laptop("apple", 45000),
+        Laptop("macbook", 60000)
+    )
 
+
+    val mychoice = laptops
+        .filter { it.price <= 5000 && it.nameOflaptop == "hp" }
+        .map { "I wanna buy this one ${it.nameOflaptop}" }
+println(mychoice)
+
+}
